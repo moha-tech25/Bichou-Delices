755d67af98bd5f9433ad9aca1cf2eae0b9b6791d
HP Probook 650 G5 <mohamedhachirou644@gmail.com>
2025-10-03 19:39:20 +0100

    Mise à jour du site

--- Script.js
+++ Script.js
@@ -1,6 +1,14 @@
-const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('.nav');
+
+const hamburger = document.querySelector('.hamburger'); // sélectionne le bouton hamburger
+const nav = document.querySelector('.nav');             // sélectionne la nav principale
 
 hamburger.addEventListener('click', () => {
-    nav.classList.toggle('active');
+    nav.classList.toggle('active');                     // ajoute ou enlève la classe "active"
+});
+const navLinks = document.querySelectorAll('.nav a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.remove('active'); // ferme le menu après sélection
+    });
 });
